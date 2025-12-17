378bc57ac14f6023fff6f47ae3b571b4525620c1
simensae1 <tsang.simeon@gmail.com>
2025-12-17 14:14:26 +0100

    modified:   sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -128,7 +128,7 @@ class MonthlySplit(BaseCrossValidator):
         for which this column is not a datetime, it will raise a ValueError.
         To use the index as column just set `time_col` to `'index'`.
     """
-    def __init__(self, time_col='index'):  # noqa: D107
+    def __init__(self, time_col='index'):
         self.time_col = time_col
     def get_n_splits(self, X, y=None, groups=None):
         """Return the number of splitting iterations in the cross-validator.
@@ -179,15 +179,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
-        n_samples = X.shape[0]
-        n_splits = self.get_n_splits(X, y, groups)
-        for i in range(n_splits):
-            idx_train = range(n_samples)
-            idx_test = range(n_samples)
-            yield (
-                idx_train, idx_test
-            )
         if self.time_col == "index":
             if not isinstance(X.index, pd.DatetimeIndex):
                 raise ValueError(
